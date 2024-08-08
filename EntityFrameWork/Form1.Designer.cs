687531bcbc0d2f6cb3126efeb7f425680774af8b
mmoa8063@gmail.com <mmoa8063@gmail.com>
2024-08-08 21:33:41 +0330

    Add project files.

---
+++ EntityFrameWork/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace EntityFrameWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsertWithEF = new System.Windows.Forms.Button();
+            this.btnSelectWithEF = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInsertWithEF
+            // 
+            this.btnInsertWithEF.Location = new System.Drawing.Point(12, 12);
+            this.btnInsertWithEF.Name = "btnInsertWithEF";
+            this.btnInsertWithEF.Size = new System.Drawing.Size(114, 23);
+            this.btnInsertWithEF.TabIndex = 0;
+            this.btnInsertWithEF.Text = "Insert With EF";
+            this.btnInsertWithEF.UseVisualStyleBackColor = true;
+            this.btnInsertWithEF.Click += new System.EventHandler(this.btnInsertWithEF_Click);
+            // 
+            // btnSelectWithEF
+            // 
+            this.btnSelectWithEF.Location = new System.Drawing.Point(146, 12);
+            this.btnSelectWithEF.Name = "btnSelectWithEF";
+            this.btnSelectWithEF.Size = new System.Drawing.Size(114, 23);
+            this.btnSelectWithEF.TabIndex = 1;
+            this.btnSelectWithEF.Text = "Select With EF";
+            this.btnSelectWithEF.UseVisualStyleBackColor = true;
+            this.btnSelectWithEF.Click += new System.EventHandler(this.btnSelectWithEF_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 51);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(425, 387);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(282, 12);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(114, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete With EF";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(443, 14);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 4;
+            this.txtId.TextChanged += new System.EventHandler(this.txtId_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(413, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ID :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSelectWithEF);
+            this.Controls.Add(this.btnInsertWithEF);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsertWithEF;
+        private System.Windows.Forms.Button btnSelectWithEF;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ EntityFrameWork/Form1.cs
@@ -0,0 +1,73 @@
+﻿using EntityFrameWork.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsertWithEF_Click(object sender, EventArgs e)
+        {
+            using (var dbContext=new CsharpSampleDBEntities())
+            {
+                var student = new Student()
+                {
+                    FirstName = "Morteza",
+                    LastName = "Chegeni",
+                    NationalCode = 3962543600,
+                    Gender = true
+                };
+                dbContext.Students.Add(student);
+                dbContext.SaveChanges();
+                MessageBox.Show("Insert Succ....");
+            }
+        }
+
+        private void btnSelectWithEF_Click(object sender, EventArgs e)
+        {
+         using (var dbContext=new CsharpSampleDBEntities())
+            {
+                var result = dbContext.Students.OrderBy(x => x.FirstName)
+                .Select(x => new { x.Id, FullName = x.FirstName + " " + x.LastName, x.NationalCode });
+
+                dataGridView1.DataSource=result.ToList();
+            }
+        }
+
+        private void txtId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+
+            using (var db = new CsharpSampleDBEntities())
+            {
+                Student item = db.Students.Find(id);
+                //Student item = db.Students.First(q => q.Id == id);
+                //Student item = db.Students.FirstOrDefault(q => q.Id == id);
+                //Student item = db.Students.Single(q => q.Id == id);
+                //Student item = db.Students.SingleOrDefault(q => q.Id == id);
+
+                db.Students.Remove(item);
+                db.SaveChanges();
+                MessageBox.Show("Delete Succ.....");
+            }
+        }
+    }
+}
+    
